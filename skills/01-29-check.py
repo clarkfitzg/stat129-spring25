e52917905e0f7e2435c221d0042d1da33c6cc8b2
Clark Fitzgerald <fitzgerald@nsm-stats.csus.edu>
2025-01-29 19:45:56 +0000

    first practice quiz

---
+++ skills/01-29-check.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import filecmp
+
+home = Path.home()
+
+skills = home / "skills"
+if skills.is_dir():
+    print("Q1 correct")
+else:
+    print("Q1 try again")
+
+
+d = skills / "01-29"
+if d.is_dir():
+    print("Q2 correct")
+else:
+    print("Q2 try again")
+
+
+f = d / "q3.txt"
+if filecmp.cmp(f, "/stat129/class/skills/scratch/01-29.out"):
+    print("Q3 correct")
+else:
+    print("Q3 try again")
